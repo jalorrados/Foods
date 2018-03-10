21a468b337020b235fb54944dc9e453eda097b0b
chrisvk <chrisvktorrejon@gmail.com>
2018-03-10 17:56:50 +0100

    Pagina principal
    Diseño resposive 97%

--- application/views/Inicio.php
+++ application/views/Inicio.php
@@ -10,7 +10,7 @@
 		<button type="button" class="btn btn-primary mx-auto botones" style="width: 150px;">Prueba</button>
 	</div>
 
-	<div class="imagen2 text-center row align-content-center justify-content-center ml-1 mt-0 mt-md-5">
+	<div class="imagen2 text-center row align-content-center justify-content-center mx-auto mt-5">
 		<div class="card col-sm-12 col-md-3" style="width: 18rem; height: 25rem;">
 		  <img class="card-img-top2 mx-auto" src="<?= base_url() ?>assets/img/icon1.svg" alt="Card image cap">
 		  <div class="card-body2">
@@ -40,7 +40,7 @@
 		<button type="button" class="btn btn-primary mx-auto botones" style="width: 150px;">Prueba</button>
 	</div>
 
-	<div class="imagen4 text-center row align-content-center justify-content-center ml-1">
+	<div class="imagen4 text-center row align-content-center justify-content-center mx-auto my-5">
 	  <p class="h2 mx-auto col-12 ">Acerca de</p>
 	  <div class="col-12">
 	    <h5 class="card-title">Prueba de texto</h5>
