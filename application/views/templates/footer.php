932ec6d18d707e8ec03b63f899c5efb9c5c83b45
chrisvk <chrisvktorrejon@gmail.com>
2018-03-10 17:46:19 +0100

    Diseño responsive
    Diseño responsive de la pagina principal a 95%

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -2,22 +2,22 @@
   <div class="fondogris pt-4">
     <h5 class="h3 text-white mx-auto text-center">Prueba de texto</h5><br/>
     <div class="row mx-auto text-center">
-      <div class="col-3">
+      <div class="col-xs-12 col-md-3 my-md-4">
         <ul class="list-unstyled">
           <li><a class="white-text text-lighten-3 text-white" href="#!">Categoria 1</a></li>
         </ul>
       </div>
-      <div class="col-3">
+      <div class="col-xs-12 col-md-3 my-md-4">
         <ul class="list-unstyled">
           <li><a class="white-text text-lighten-3 text-white" href="#!">Categoria 2</a></li>
         </ul>
       </div>
-      <div class="col-3">
+      <div class="col-xs-12 col-md-3 my-md-4">
         <ul class="list-unstyled">
           <li><a class="white-text text-lighten-3 text-white" href="#!">Categoria 3</a></li>
         </ul>
       </div>
-      <div class="col-3">
+      <div class="col-xs-12 col-md-3 my-md-4">
         <ul class="list-unstyled">
           <li><a class="white-text text-lighten-3 text-white" href="#!">Categoria 4</a></li>
         </ul>
