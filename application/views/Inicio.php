c9356a59b6e7bc241667bc9ccab6689b32bfddcb
chrisvk <chrisvktorrejon@gmail.com>
2018-03-10 17:11:58 +0100

    Diseño responsive
    Adaptado al 90%

--- application/views/Inicio.php
+++ application/views/Inicio.php
@@ -1,16 +1,13 @@
 <section>
 
 	<div class="imagenblanco">
-			<!--<span class="white-text principal">Prueba de texto</span><br/>
-			<span class="white-text secundario">Prueba de texto 2</span><br/><br/>
-			<button type="button" class="waves-effect waves-light btn-large">Prueba</button>-->
+
 	</div>
 
 	<div class="imagen1 btn-group-vertical" id="navscrollstart">
-		<span class="h1 mx-auto">Prueba de texto</span><br/>
-		<span class="h5 mx-auto text-white">Prueba de texto 2</span><br/><br/>
+		<span class="mx-auto">Prueba de texto</span><br/>
+		<span class="mx-auto text-white">Prueba de texto 2</span><br/><br/>
 		<button type="button" class="btn btn-primary mx-auto botones" style="width: 150px;">Prueba</button>
-	
 	</div>
 
 	<div class="imagen2 text-center row align-content-center justify-content-center ml-1 mt-0 mt-md-5">
@@ -37,35 +34,18 @@
 		</div>
 	</div>
 
-	<div class="imagen3 text-center m-y-auto text-white" id="navscrollstart">
-		<span class="h1">Prueba de texto</span><br/>
-		<span class="">Prueba de texto 2</span><br/><br/>
-		<button type="button" class="waves-effect waves-light btn-large">Prueba</button>
-	
+	<div class="imagen3 btn-group-vertical text-white" id="navscrollstart">
+		<span class="mx-auto">Prueba de texto</span><br/>
+		<span class="mx-auto text-white">Prueba de texto 2</span><br/><br/>
+		<button type="button" class="btn btn-primary mx-auto botones" style="width: 150px;">Prueba</button>
 	</div>
 
-	<div class="imagen2 text-center row align-content-center justify-content-center ml-1 mt-0 mt-md-5">
-		<div class="card col-sm-12 col-md-3" style="width: 18rem; height: 25rem;">
-		  <img class="card-img-top mx-auto" src="<?= base_url() ?>assets/img/icon1.svg" alt="Card image cap">
-		  <div class="card-body">
-		    <h5 class="card-title">Prueba de texto</h5>
-		    <p class="card-text">We did most of the heavy lifting for you to provide a default stylings that incorporate our custom components.</p>
-		  </div>
-		</div>
-		<div class="card col-sm-12 col-md-3" style="width: 18rem; height: 25rem;">
-		  <img class="card-img-top mx-auto" src="<?= base_url() ?>assets/img/icon2.svg" alt="Card image cap">
-		  <div class="card-body">
-		    <h5 class="card-title">Prueba de texto</h5>
-		    <p class="card-text">We did most of the heavy lifting for you to provide a default stylings that incorporate our custom components.</p>
-		  </div>
-		</div>
-		<div class="card col-sm-12 col-md-3" style="width: 18rem; height: 25rem;">
-		  <img class="card-img-top mx-auto" src="<?= base_url() ?>assets/img/icon3.svg" alt="Card image cap">
-		  <div class="card-body">
-		    <h5 class="card-title">Prueba de texto</h5>
-		    <p class="card-text">We did most of the heavy lifting for you to provide a default stylings that incorporate our custom components.</p>
-		  </div>
-		</div>
+	<div class="imagen4 text-center row align-content-center justify-content-center ml-1">
+	  <p class="h1 mx-auto col-12 ">Acerca de</p>
+	  <div class="col-12">
+	    <h5 class="card-title">Prueba de texto</h5>
+	    <p class="card-text">We did most of the heavy lifting for you to provide a default stylings that incorporate our custom components.</p>
+	  </div>
 	</div>
 
 </section>
