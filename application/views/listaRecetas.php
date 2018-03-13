6032e51907c5432b6d8d18c14ee3201604753418
jalorrados <jalorrados@users.noreply.github.com>
2018-03-13 11:52:38 +0100

    Creada la lista de recetas

---
+++ application/views/listaRecetas.php
@@ -0,0 +1,31 @@
+<div class="container my-5">
+    <div class="row">
+        <div class="col-12 mt-5 mb-3 my-3">
+            <div class="media">
+              <div class="media-left">
+                <a href="#">
+                  <img class="media-object" src="<?= base_url() ?>assets/img/aperitivos_tapas.jpg">
+                </a>
+              </div>
+              <div class="media-body ml-3">
+                <h4 class="media-heading">Media heading</h4>
+                Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis.
+              </div>
+            </div>
+        </div>
+        <hr width="100%" color="black"/>
+        <div class="col-12 my-3">
+            <div class="media">
+              <div class="media-left">
+                <a href="#">
+                  <img class="media-object" src="<?= base_url() ?>assets/img/aperitivos_tapas.jpg">
+                </a>
+              </div>
+              <div class="media-body ml-3">
+                <h4 class="media-heading">Media heading</h4>
+                Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis.
+              </div>
+            </div>
+        </div>
+    </div>
+</div>
